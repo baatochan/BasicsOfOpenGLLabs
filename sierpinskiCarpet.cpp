62c0d027d2a8fc0af81164cb596bdb09864737df
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2017-10-29 19:48:12 +0100

    Added perurbations and comments
    Comments have to be in Polish... Don't judge me :/

--- sierpinskiCarpet.cpp
+++ sierpinskiCarpet.cpp
@@ -9,29 +9,44 @@
 
 using namespace std;
 
-int perturbationLevel; // Poziom deformacji dywanu (0-20)
+int perturbationLevel; // Poziom deformacji dywanu (0-5)
 int numberOfStepsToDraw; // Ilosc krokow dywanu do narysowania (0-5)
 bool askUser; // Czy pytac użytwkownika o wartosci powyzszych zmiennych
 
+// funckja zwracajaca losowa wartosc z zakresu [0,1]; przydatna do podawania losowych kolorow
 float getRandomColor() {
-    int random = rand() % 10000;
-    float color = (float)random / 10000;
+    int random = rand() % 10001; //generuj losowa wartosc z zakresu [0, 10000]
+    float color = (float)random / 10000; // podziel na 10k, wiec zakres zmienia sie na [0,1]
 
     return color;
 }
 
+//funckja zwracajaca losowa wartosc z zakresu [-side/4, side/4]
+float getRandomOffset(float side) {
+    int random = (rand() % 201) - 100; // generuj losowa wartosc z zakresu [-100,100]
+    random *= perturbationLevel; //pomnoz to przez poziom znieksztalcen
+
+    int maxOffset = 2000; // maksymalna wartosc 4*random*perturbationLevel
+
+    float result = (float)random * side;
+    result /= (float)maxOffset; // result moza przyjmowac wartosci [-side/4, side/4]
+
+    return result;
+}
+
 // Rekurencyjna funkcja rysujaca dywan
 void drawCarpet(int numberOfStepsToDraw, point2 leftTopCorner, point2 rightBottomCorner) {
-    float sideOfSquare = rightBottomCorner[0] - leftTopCorner[0];
-    if (numberOfStepsToDraw == 0) {
-        point2 finalLeftTopCorner = { leftTopCorner[0], leftTopCorner[1] };
-        point2 finalRightTopCorner = { rightBottomCorner[0], leftTopCorner[1] };
-        point2 finalLeftBottomCorner = { leftTopCorner[0], rightBottomCorner[1] };
-        point2 finalRightBottomCorner = { rightBottomCorner[0], rightBottomCorner[1] };
+    float sideOfSquare = rightBottomCorner[0] - leftTopCorner[0]; // bok kwadratu
+    if (numberOfStepsToDraw == 0) { // gdy poziom rekurencji wynosi 0 => narysuj kwadrat
+        // wartosci wspolrzednych kwadratu do nmarysowania z uwzglednieniem przesuniecia w wyniku perturbacji
+        point2 finalLeftTopCorner = { leftTopCorner[0] + getRandomOffset(sideOfSquare), leftTopCorner[1] + getRandomOffset(sideOfSquare) };
+        point2 finalRightTopCorner = { rightBottomCorner[0] + getRandomOffset(sideOfSquare), leftTopCorner[1] + getRandomOffset(sideOfSquare) };
+        point2 finalLeftBottomCorner = { leftTopCorner[0] + getRandomOffset(sideOfSquare), rightBottomCorner[1] + getRandomOffset(sideOfSquare) };
+        point2 finalRightBottomCorner = { rightBottomCorner[0] + getRandomOffset(sideOfSquare), rightBottomCorner[1] + getRandomOffset(sideOfSquare) };
 
         glBegin(GL_QUADS);
-            glColor3f(getRandomColor(), getRandomColor(), getRandomColor());
-            glVertex2fv(finalRightTopCorner);
+            glColor3f(getRandomColor(), getRandomColor(), getRandomColor()); // ustaw losowy kolor
+            glVertex2fv(finalRightTopCorner); // ustaw wierzcholek
 
             glColor3f(getRandomColor(), getRandomColor(), getRandomColor());
             glVertex2fv(finalRightBottomCorner);
@@ -44,29 +59,25 @@ void drawCarpet(int numberOfStepsToDraw, point2 leftTopCorner, point2 rightBotto
         glEnd();
 
         return;
-    } else if (numberOfStepsToDraw < 0) {
+    } else if (numberOfStepsToDraw < 0) { // jesli poziom rekurencji jest ponizej 0 wyrzuc blad
         cout<<"Cos jest nie tak. Aplikacja nie moze kontynuowac."<<endl;
         return;
     } else {
-        numberOfStepsToDraw--;
-        point2 leftTopOfSmaller;
+        numberOfStepsToDraw--; // zmniejsz poziom rekurencji
+        point2 leftTopOfSmaller; // deklaracja wierzcholkow dla mniejszych kwadratow
         point2 rightBottomOfSmaller;
-        float sideOfSmallerSquare = sideOfSquare/3;
-        //cout<<sideOfSmallerSquare<<" "<<sideOfSquare<<endl;
-        //int xxx = 0;
+        float sideOfSmallerSquare = sideOfSquare/3; // wyliczenie dlugosci wierzcholka mniejszego kwadratu
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                //xxx++;
-                if (i == 1 && j == 1) continue;
+                if (i == 1 && j == 1) continue; // pomin srodkowy
+                // wyliczenia wspolrzednych kolejnych mniejszych kwadratow
                 leftTopOfSmaller[0] = leftTopCorner[0] + j * sideOfSmallerSquare;
                 leftTopOfSmaller[1] = leftTopCorner[1] - i * sideOfSmallerSquare;
                 rightBottomOfSmaller[0] = (rightBottomCorner[0] - sideOfSquare) + (j+1) * sideOfSmallerSquare;
                 rightBottomOfSmaller[1] = (rightBottomCorner[1] + sideOfSquare) - (i+1) * sideOfSmallerSquare;
-                //std::cout <<xxx<< " "<< rightBottomOfSmaller[0] << " " << rightBottomOfSmaller[1] << std::endl;
-                //std::cout <<xxx<< " "<< leftTopOfSmaller[0] << " " << leftTopOfSmaller[1] << " " << rightBottomOfSmaller[0] << " " << rightBottomOfSmaller[1] << std::endl;
-                drawCarpet(numberOfStepsToDraw, leftTopOfSmaller, rightBottomOfSmaller);
+                drawCarpet(numberOfStepsToDraw, leftTopOfSmaller, rightBottomOfSmaller); // wywolanie rekurencyjnej funkcji rusyjacej mneisjzy kwadrat
             }
         }
         return;
@@ -79,12 +90,12 @@ void RenderScene(void)
 {
     glClear(GL_COLOR_BUFFER_BIT); // Czyszczenie okna aktualnym kolorem czyszczącym
 
-    glColor3f(1.0f, 0.0f, 0.0f);
+    glColor3f(1.0f, 0.0f, 0.0f); // ustawienie koloru na czerwony (potem i tak zmieniany na losowy)
 
-    point2 leftTopCorner = { -80.0f, 80.0f };
-    point2 rightBottomCorner = { 80.0f, -80.0f };
+    point2 leftTopCorner = { -80.0f, 80.0f }; // lewy gorny wierzcholek dywanu
+    point2 rightBottomCorner = { 80.0f, -80.0f }; // prawy dolny wierzcholek dywanu
 
-    drawCarpet(numberOfStepsToDraw, leftTopCorner, rightBottomCorner);
+    drawCarpet(numberOfStepsToDraw, leftTopCorner, rightBottomCorner); // wywolanie funkcji rysujacej dywan
 
     glFlush(); // Przekazanie poleceń rysujących do wykonania
 }
@@ -92,8 +103,8 @@ void RenderScene(void)
 // Funkcja ustalająca stan renderowania
 void MyInit(void)
 {
-    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-    // Kolor okna wnętrza okna - ustawiono na szary
+    glClearColor(0.969f, 0.725f, 0.808f, 1.0f);
+    // Kolor okna wnętrza okna - ustawiono na kolor grzywy Fluttershy c:
 }
 
 // Funkcja służąca do kontroli zachowania proporcji rysowanych obiektów
@@ -137,21 +148,21 @@ void ChangeSize(GLsizei horizontal, GLsizei vertical)
 int main(int argc, char* argv[])
 {
     srand(time(0)); // ziarno dla liczb pseudo losowych
-    askUser = false;
+    askUser = true; // czy pytac uzytkownika o stopien zdeformowania i ilosc krokow
 
     if (askUser) {
         do {
-            cout << "Podaj stopien zdeformowania (0-20):";
+            cout << "Podaj stopien zdeformowania (0-5):";
             cin >> perturbationLevel;
-        } while (perturbationLevel < 0 || perturbationLevel > 20);
+        } while (perturbationLevel < 0 || perturbationLevel > 5); // stopien zdeformowania moze miec wartosci od 0 do 5
 
         do {
             cout << "Podaj ilosc krokow (poziom rekurencji) (0-5):";
             cin >> numberOfStepsToDraw;
-        } while (numberOfStepsToDraw < 0 || numberOfStepsToDraw > 5);
+        } while (numberOfStepsToDraw < 0 || numberOfStepsToDraw > 5); // ilosc krokow moze miec wartosc od 0 do 5
     } else {
         // Jesli chcesz podac dane bez pytania uzytkownika ustaw askUser na false i podaj tu dane
-        perturbationLevel = 0;
+        perturbationLevel = 3;
         numberOfStepsToDraw = 3;
     }
 
