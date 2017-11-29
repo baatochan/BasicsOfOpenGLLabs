534b7c2089cad977c568330fdf14b95db202ce58
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2017-11-29 11:02:39 +0100

    Convert intends to tabs

--- grafika/main.cpp
+++ grafika/main.cpp
@@ -31,34 +31,34 @@ static int delta_zoom = 0;        // różnica pomiędzy pozycją bieżącą (zo
 
 void Axes(void)
 {
-    point3  x_min = {-5.0, 0.0, 0.0};
-    point3  x_max = { 5.0, 0.0, 0.0};
+	point3  x_min = {-5.0, 0.0, 0.0};
+	point3  x_max = { 5.0, 0.0, 0.0};
 // pocz?tek i koniec obrazu osi x
 
-    point3  y_min = {0.0, -5.0, 0.0};
-    point3  y_max = {0.0,  5.0, 0.0};
+	point3  y_min = {0.0, -5.0, 0.0};
+	point3  y_max = {0.0,  5.0, 0.0};
 // pocz?tek i koniec obrazu osi y
 
-    point3  z_min = {0.0, 0.0, -5.0};
-    point3  z_max = {0.0, 0.0,  5.0};
+	point3  z_min = {0.0, 0.0, -5.0};
+	point3  z_max = {0.0, 0.0,  5.0};
 //  pocz?tek i koniec obrazu osi y
-    glColor3f(1.0f, 0.0f, 0.0f);  // kolor rysowania osi - czerwony
-    glBegin(GL_LINES); // rysowanie osi x
-    glVertex3fv(x_min);
-    glVertex3fv(x_max);
-    glEnd();
+	glColor3f(1.0f, 0.0f, 0.0f);  // kolor rysowania osi - czerwony
+	glBegin(GL_LINES); // rysowanie osi x
+	glVertex3fv(x_min);
+	glVertex3fv(x_max);
+	glEnd();
 
-    glColor3f(0.0f, 1.0f, 0.0f);  // kolor rysowania - zielony
-    glBegin(GL_LINES);  // rysowanie osi y
-    glVertex3fv(y_min);
-    glVertex3fv(y_max);
-    glEnd();
+	glColor3f(0.0f, 1.0f, 0.0f);  // kolor rysowania - zielony
+	glBegin(GL_LINES);  // rysowanie osi y
+	glVertex3fv(y_min);
+	glVertex3fv(y_max);
+	glEnd();
 
-    glColor3f(0.0f, 0.0f, 1.0f);  // kolor rysowania - niebieski
-    glBegin(GL_LINES); // rysowanie osi z
-    glVertex3fv(z_min);
-    glVertex3fv(z_max);
-    glEnd();
+	glColor3f(0.0f, 0.0f, 1.0f);  // kolor rysowania - niebieski
+	glBegin(GL_LINES); // rysowanie osi z
+	glVertex3fv(z_min);
+	glVertex3fv(z_max);
+	glEnd();
 }
 /*************************************************************************************/
 // Funkcja określająca co ma być rysowane (zawsze wywoływana, gdy trzeba
@@ -66,40 +66,40 @@ void Axes(void)
 
 void RenderScene(void)
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 // Czyszczenie okna aktualnym kolorem czyszczącym
 
-    glLoadIdentity();
+	glLoadIdentity();
 // Czyszczenie macierzy bie??cej
 
-    if (status == 2) {
-        viewer[2] = viewer[2] - 2*delta_zoom;
-        if (viewer[2] < 8) viewer[2] = 8;
-        if (viewer[2] > 900) viewer[2] = 900;
-    }
+	if (status == 2) {
+		viewer[2] = viewer[2] - 2*delta_zoom;
+		if (viewer[2] < 8) viewer[2] = 8;
+		if (viewer[2] > 900) viewer[2] = 900;
+	}
 
-    gluLookAt(viewer[0],viewer[1],viewer[2], 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	gluLookAt(viewer[0],viewer[1],viewer[2], 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 // Zdefiniowanie położenia obserwatora
-    Axes();
+	Axes();
 // Narysowanie osi przy pomocy funkcji zdefiniowanej powyżej
 
-    if(status == 1)                     // jeśli lewy klawisz myszy wcięnięty
-    {
-        thetaX += delta_x*pix2angle;    // modyfikacja kąta obrotu o kat proporcjonalny
-        thetaY += delta_y*pix2angle;    // modyfikacja kąta obrotu o kat proporcjonalny
-    }                                  // do różnicy położeń kursora myszy
+	if(status == 1)                     // jeśli lewy klawisz myszy wcięnięty
+	{
+		thetaX += delta_x*pix2angle;    // modyfikacja kąta obrotu o kat proporcjonalny
+		thetaY += delta_y*pix2angle;    // modyfikacja kąta obrotu o kat proporcjonalny
+	}                                  // do różnicy położeń kursora myszy
 
-    glRotatef(thetaX, 0.0, 1.0, 0.0);  //obrót obiektu o nowy kąt
-    glRotatef(thetaY, 1.0, 0.0, 0.0);
+	glRotatef(thetaX, 0.0, 1.0, 0.0);  //obrót obiektu o nowy kąt
+	glRotatef(thetaY, 1.0, 0.0, 0.0);
 
-    glColor3f(1.0f, 1.0f, 1.0f);
+	glColor3f(1.0f, 1.0f, 1.0f);
 // Ustawienie koloru rysowania na biały
 
-    glutWireTeapot(3.0);
+	glutWireTeapot(3.0);
 // Narysowanie czajnika
-    glFlush();
+	glFlush();
 // Przekazanie poleceń rysujących do wykonania
-    glutSwapBuffers();
+	glutSwapBuffers();
 
 }
 /*************************************************************************************/
@@ -107,7 +107,7 @@ void RenderScene(void)
 
 void MyInit(void)
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 // Kolor czyszczący (wypełnienia okna) ustawiono na czarny
 }
 /*************************************************************************************/
@@ -119,31 +119,31 @@ void MyInit(void)
 
 void ChangeSize(GLsizei horizontal, GLsizei vertical)
 {
-    pix2angle = 360.0/(float)horizontal;  // przeliczenie pikseli na stopnie
+	pix2angle = 360.0/(float)horizontal;  // przeliczenie pikseli na stopnie
 
-    glMatrixMode(GL_PROJECTION);
-    // Przełączenie macierzy bieżącej na macierz projekcji
+	glMatrixMode(GL_PROJECTION);
+	// Przełączenie macierzy bieżącej na macierz projekcji
 
-    glLoadIdentity();
-    // Czyszcznie macierzy bieżącej
+	glLoadIdentity();
+	// Czyszcznie macierzy bieżącej
 
-    gluPerspective(70, 1.0, 1.0, 1000.0);
-    // Ustawienie parametrów dla rzutu perspektywicznego
+	gluPerspective(70, 1.0, 1.0, 1000.0);
+	// Ustawienie parametrów dla rzutu perspektywicznego
 
 
-    if(horizontal <= vertical)
-        glViewport(0, (vertical-horizontal)/2, horizontal, horizontal);
+	if(horizontal <= vertical)
+		glViewport(0, (vertical-horizontal)/2, horizontal, horizontal);
 
-    else
-        glViewport((horizontal-vertical)/2, 0, vertical, vertical);
-    // Ustawienie wielkości okna okna widoku (viewport) w zależności
-    // relacji pomiędzy wysokością i szerokością okna
+	else
+		glViewport((horizontal-vertical)/2, 0, vertical, vertical);
+	// Ustawienie wielkości okna okna widoku (viewport) w zależności
+	// relacji pomiędzy wysokością i szerokością okna
 
-    glMatrixMode(GL_MODELVIEW);
-    // Przełączenie macierzy bieżącej na macierz widoku modelu
+	glMatrixMode(GL_MODELVIEW);
+	// Przełączenie macierzy bieżącej na macierz widoku modelu
 
-    glLoadIdentity();
-    // Czyszczenie macierzy bieżącej
+	glLoadIdentity();
+	// Czyszczenie macierzy bieżącej
 
 }
 /*************************************************************************************/
@@ -153,19 +153,19 @@ void Mouse(int btn, int state, int x, int y)
 {
 
 
-    if(btn==GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-    {
-        x_pos_old=x;         // przypisanie aktualnie odczytanej pozycji kursora
-        y_pos_old=y;         // przypisanie aktualnie odczytanej pozycji kursora
-        // jako pozycji poprzedniej
-        status = 1;          // wcięnięty został lewy klawisz myszy
-    } else if (btn==GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-        zoom_pos_old=x;
-        status = 2;
-    }
-    else
+	if(btn==GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		x_pos_old=x;         // przypisanie aktualnie odczytanej pozycji kursora
+		y_pos_old=y;         // przypisanie aktualnie odczytanej pozycji kursora
+		// jako pozycji poprzedniej
+		status = 1;          // wcięnięty został lewy klawisz myszy
+	} else if (btn==GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+		zoom_pos_old=x;
+		status = 2;
+	}
+	else
 
-        status = 0;          // nie został wcięnięty żaden klawisz
+		status = 0;          // nie został wcięnięty żaden klawisz
 }
 
 /*************************************************************************************/
@@ -175,19 +175,19 @@ void Mouse(int btn, int state, int x, int y)
 void Motion( GLsizei x, GLsizei y )
 {
 
-    delta_x=x-x_pos_old;     // obliczenie różnicy położenia kursora myszy
+	delta_x=x-x_pos_old;     // obliczenie różnicy położenia kursora myszy
 
-    x_pos_old=x;            // podstawienie bieżącego położenia jako poprzednie
+	x_pos_old=x;            // podstawienie bieżącego położenia jako poprzednie
 
-    delta_y=y-y_pos_old;     // obliczenie różnicy położenia kursora myszy
+	delta_y=y-y_pos_old;     // obliczenie różnicy położenia kursora myszy
 
-    y_pos_old=y;            // podstawienie bieżącego położenia jako poprzednie
+	y_pos_old=y;            // podstawienie bieżącego położenia jako poprzednie
 
-    delta_zoom=x-zoom_pos_old;     // obliczenie różnicy położenia kursora myszy
+	delta_zoom=x-zoom_pos_old;     // obliczenie różnicy położenia kursora myszy
 
-    zoom_pos_old=x;            // podstawienie bieżącego położenia jako poprzednie
+	zoom_pos_old=x;            // podstawienie bieżącego położenia jako poprzednie
 
-    glutPostRedisplay();     // przerysowanie obrazu sceny
+	glutPostRedisplay();     // przerysowanie obrazu sceny
 }
 
 /*************************************************************************************/
@@ -197,36 +197,36 @@ void Motion( GLsizei x, GLsizei y )
 
 int main(int argc, char* argv[])
 {
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB |GLUT_DEPTH);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB |GLUT_DEPTH);
 
-    glutInit(&argc, argv);
+	glutInit(&argc, argv);
 
-    glutInitWindowSize(300, 300);
+	glutInitWindowSize(300, 300);
 
-    glutCreateWindow("Rzutowanie perspektywiczne");
+	glutCreateWindow("Rzutowanie perspektywiczne");
 
-    glutDisplayFunc(RenderScene);
+	glutDisplayFunc(RenderScene);
 // Określenie, że funkcja RenderScene będzie funkcją zwrotną
 // (callback function).  Będzie ona wywoływana za każdym razem
 // gdy zajdzie potrzeba przerysowania okna
 
-    glutReshapeFunc(ChangeSize);
+	glutReshapeFunc(ChangeSize);
 // Dla aktualnego okna ustala funkcję zwrotną odpowiedzialną
 // za zmiany rozmiaru okna
 
-    MyInit();
+	MyInit();
 // Funkcja MyInit() (zdefiniowana powyżej) wykonuje wszelkie
 // inicjalizacje konieczne  przed przystąpieniem do renderowania
-    glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 // Włączenie mechanizmu usuwania niewidocznych elementów sceny
-    glutMouseFunc(Mouse);
+	glutMouseFunc(Mouse);
 // Ustala funkcję zwrotną odpowiedzialną za badanie stanu myszy
 
-    glutMotionFunc(Motion);
+	glutMotionFunc(Motion);
 // Ustala funkcję zwrotną odpowiedzialną za badanie ruchu myszy
-    glutMainLoop();
+	glutMainLoop();
 // Funkcja uruchamia szkielet biblioteki GLUT
 
-    return 0;
+	return 0;
 }
 /*************************************************************************************/
