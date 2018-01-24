7db1bd16f8ff4d532d4f42c92948d4440eb49835
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-01-24 09:36:30 +0100

    Add webGL first task

---
+++ WebGL/js/main.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("glcanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#EE7070"; // red
+ctx.fillRect(50, 50, 160, 80);
+ctx.fillStyle = "#70CB55"; // green
+ctx.fillRect(90, 80, 140, 70);
+ctx.fillStyle = "#5C79AB"; // blue
+ctx.fillRect(130, 110, 120, 60);
